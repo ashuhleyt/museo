3ce17ed46179976e09fd4e61c219398e7a5e90f4
Ashley <ashuhleyt@gmail.com>
2022-09-15 08:34:05 -0700

    feat: add_photograph

--- lib/curator.rb
+++ lib/curator.rb
@@ -4,4 +4,8 @@ class Curator
   def initialize
     @photographs = []
   end
+
+  def add_photograph(photograph)
+    @photographs << photograph
+  end
 end
--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -6,13 +6,31 @@ require './lib/curator'
 RSpec.describe Curator do 
   before(:each) do 
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+      id: "1",      
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+      artist_id: "1",      
+      year: "1954"      
+ })
+    @photo_2 = Photograph.new({
+      id: "2",      
+      name: "Moonrise, Hernandez",      
+      artist_id: "2",      
+      year: "1941"      
+ }) 
   end
 
   it 'exists' do 
     expect(@curator).to be_instance_of(Curator)
   end
 
-  it 'starts with no photographs' do 
+  it 'starts with no photographs and can add photographs' do 
     expect(@curator.photographs).to eq([])
+    
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    expect(@curator.photographs).to eq([@photo_1, @photo_2])
   end
+
 end
